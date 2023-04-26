e8d657c088dd04993b09b545e422837df7d1b39e
Manuel SHARIFI <msharifi@e2r10p2.clusters.42paris.fr>
2023-04-26 18:32:02 +0200

    [CREATE] all files

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msharifi <msharifi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 18:26:58 by msharifi          #+#    #+#             */
+/*   Updated: 2023/04/26 18:28:27 by msharifi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class	Zombie
+{
+	private:
+
+	public:
+		Zombie(void);	
+		~Zombie(void);	
+};
+
+#endif
